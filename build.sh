ade69a6a2f0a9dbdfe92e7b4244e9a7d24406690
Etienne Fachaux <etienne@fachaux.fr>
2017-05-22 10:20:34 +0200

    Adding Restart Always attr + version to html

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ if [ $INSTANCE_RUNNING -gt 1 ] ; then
     docker rm prod-devops
 fi
 
-docker run --name prod-devops -p 80:80 -d julespeyronnet/devopsbilly
+docker run --name prod-devops --restart=always -p 80:80 -d julespeyronnet/devopsbilly
